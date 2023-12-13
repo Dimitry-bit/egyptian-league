a8923af55be3be364497db6c1ba0366f91b5afdf
Ree37 <rimo.mrben@gmail.com>
2023-12-13 18:21:30 +0200

    setting winner team and referee availability

--- src/main/java/com/github/egyptian_league/Match.java
+++ src/main/java/com/github/egyptian_league/Match.java
@@ -8,27 +8,27 @@ public class Match {
     public final UUID matchId;
     public final UUID HomeTeamId;
     public final UUID AwayTeamId;
-    private Stadium matchStadium; // FIXME: Use a UUID
-    public final Referee Referee; // FIXME: Can it change?
+    private UUID matchStadium; // FIXME: Use a UUID
+    public Referee Referee; // FIXME: Can it change?
     private LocalDateTime dateTime;
     private HashMap<UUID, Integer> goalScorers;
+    private UUID winnerTeam;
 
-    public Match(UUID homeTeamId, UUID awayTeamId, Referee Referee) {
+    public Match(UUID homeTeamId, UUID awayTeamId) {
         matchId = UUID.randomUUID();
         this.HomeTeamId = homeTeamId;
         this.AwayTeamId = awayTeamId;
-        this.Referee = Referee;
     }
 
-    public Stadium getMatchStadium() {
+    public UUID getMatchStadium() {
         return matchStadium;
     }
 
-    public void setMatchStadium(Stadium matchStadium) {
+    public void setMatchStadium(UUID matchStadium) {
         this.matchStadium = matchStadium;
     }
 
-    private void setDate(LocalDateTime dateTime) {
+    private void SetDate(LocalDateTime dateTime) {
         // FIXME: Validate before setting
         this.dateTime = dateTime;
     }
@@ -37,12 +37,41 @@ public class Match {
         return dateTime;
     }
 
-    public HashMap<UUID, Integer> getGoalScorers() {
+    public HashMap<UUID, Integer> GetGoalScorers() {
         return goalScorers;
     }
 
-    public void addGoal(UUID playerID, int numOfGoals) {
-        goalScorers.put(playerID, numOfGoals);
+    public void AddGoal(UUID playerId, int numOfGoals) {
+        goalScorers.put(playerId, numOfGoals);
+    }
+
+    public Referee setReferee(Referee referee){
+        if (referee.CheckRefereeAvailability(this)){
+            this.Referee=referee;
+            return referee;
+        }
+        return null;
+    }
+    public void setWinnerTeam(){
+        int homeScore=0;
+        int awayScore=0;
+        for (int i=0;i<ApplicationRepository.defaultRepository.getTeamById(HomeTeamId).getPlayers().size();i++){
+            if (goalScorers.containsKey(ApplicationRepository.defaultRepository.getTeamById(HomeTeamId).getPlayers().get(i))){
+                homeScore+=goalScorers.get(ApplicationRepository.defaultRepository.getTeamById(HomeTeamId).getPlayers().get(i));
+            }
+        }
+        for (int i=0;i<ApplicationRepository.defaultRepository.getTeamById(AwayTeamId).getPlayers().size();i++){
+            if (goalScorers.containsKey(ApplicationRepository.defaultRepository.getTeamById(AwayTeamId).getPlayers().get(i))){
+                awayScore+=goalScorers.get(ApplicationRepository.defaultRepository.getTeamById(AwayTeamId).getPlayers().get(i));
+            }
+        }
+        if (homeScore>awayScore){
+            winnerTeam= HomeTeamId;
+        }
+        else if(homeScore<awayScore)
+            winnerTeam= AwayTeamId;
+        else
+            winnerTeam= null;
     }
 
     // FIXME: Why?
