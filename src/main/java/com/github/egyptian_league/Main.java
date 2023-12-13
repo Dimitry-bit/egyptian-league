5196701c9261c22da6430dd3f82a6f4eb69cd672
Tony Medhat <73913361+Dimitry-bit@users.noreply.github.com>
2023-12-13 14:17:59 +0200

    Add Arguments.java and '--no-gui' arg

--- src/main/java/com/github/egyptian_league/Main.java
+++ src/main/java/com/github/egyptian_league/Main.java
@@ -3,7 +3,13 @@ package com.github.egyptian_league;
 import com.github.egyptian_league.CLI.CLI_Input;
 
 public class Main {
-        public static void main(String[] args) {
-                CLI_Input.start();
+    public static void main(String[] args) {
+        Arguments.initArgs(args);
+
+        if (Arguments.getArgument("--no-gui", 0) != null) {
+            CLI_Input.start();
+        } else {
+            HelloApplication.launch(HelloApplication.class, args);
         }
+    }
 }
