1c520752f0b2048de093db18daadab5fd6ee5501
Tony Medhat <73913361+Dimitry-bit@users.noreply.github.com>
2023-12-19 01:25:56 +0200

    Refactor Match.java

--- src/main/java/com/github/egyptian_league/Match.java
+++ src/main/java/com/github/egyptian_league/Match.java
@@ -11,31 +11,33 @@ import com.github.egyptian_league.json.Annotations.JsonConstructor;
 import javafx.application.Application;
 
 public class Match {
-    public final UUID matchId;
-    public final UUID HomeTeamId;
-    public final UUID AwayTeamId;
-    private UUID matchStadium;
-    public Referee Referee;
+
+    public final UUID id;
+
+    private UUID homeTeamId;
+    private UUID awayTeamId;
+    private UUID stadiumId;
+    private Referee referee;
     private LocalDateTime dateTime;
-    private HashMap<UUID, Integer> goalScorers;
+    private HashMap<UUID, Integer> scorers;
 
     @JsonConstructor(parameters = { "homeTeamId", "awayTeamId" })
     public Match(UUID homeTeamId, UUID awayTeamId) {
-        matchId = UUID.randomUUID();
-        this.HomeTeamId = homeTeamId;
-        this.AwayTeamId = awayTeamId;
+        id = UUID.randomUUID();
+        this.homeTeamId = homeTeamId;
+        this.awayTeamId = awayTeamId;
     }
 
-    public UUID getMatchStadium() {
-        return matchStadium;
+    public UUID getStadiumId() {
+        return stadiumId;
     }
 
-    public void setMatchStadium(UUID matchStadium) {
-        this.matchStadium = matchStadium;
+    public void setStadiumId(UUID stadiumId) {
+        this.stadiumId = stadiumId;
     }
 
     private void setDate(LocalDateTime dateTime) {
-        Stadium stadium = ApplicationRepository.getRepository().getStadiumByUUID(matchStadium);
+        Stadium stadium = ApplicationRepository.getRepository().getStadiumByUUID(stadiumId);
         if (stadium != null && stadium.checkStadiumAvailability(dateTime)) {
             this.dateTime = dateTime;
         }
@@ -45,25 +47,25 @@ public class Match {
         return dateTime;
     }
 
-    public HashMap<UUID, Integer> getGoalScorers() {
-        return goalScorers;
+    public HashMap<UUID, Integer> getScorers() {
+        return scorers;
     }
 
     public void addGoal(UUID playerId, int numOfGoals) {
         // FIXME: Validate numbOfGoals before setting
         if (numOfGoals >= 0) {
-            goalScorers.put(playerId, numOfGoals);
+            scorers.put(playerId, numOfGoals);
         }
 
     }
 
     public void removeGoal(UUID playerId, int numOfGoals) {
-        goalScorers.put(playerId, numOfGoals);
+        scorers.put(playerId, numOfGoals);
     }
 
     public Referee setReferee(Referee referee) {
         if (referee.CheckRefereeAvailability(this)) {
-            this.Referee = referee;
+            this.referee = referee;
             return referee;
         }
         return null;
@@ -72,25 +74,25 @@ public class Match {
     public UUID calcWinnerTeam() {
         int homeScore = 0;
         int awayScore = 0;
-        Iterator<UUID> playerIdIterator = ApplicationRepository.getRepository().getTeamById(HomeTeamId).getPlayers();
+        Iterator<UUID> playerIdIterator = ApplicationRepository.getRepository().getTeamById(homeTeamId).getPlayers();
         while (playerIdIterator.hasNext()) {
             UUID playerUUID = playerIdIterator.next();
-            if (goalScorers.containsKey(playerUUID)) {
-                homeScore += goalScorers.get(playerUUID);
+            if (scorers.containsKey(playerUUID)) {
+                homeScore += scorers.get(playerUUID);
             }
         }
-        playerIdIterator = ApplicationRepository.getRepository().getTeamById(AwayTeamId).getPlayers();
+        playerIdIterator = ApplicationRepository.getRepository().getTeamById(awayTeamId).getPlayers();
         while (playerIdIterator.hasNext()) {
             UUID playerUUID = playerIdIterator.next();
-            if (goalScorers.containsKey(playerUUID)) {
-                awayScore += goalScorers.get(playerUUID);
+            if (scorers.containsKey(playerUUID)) {
+                awayScore += scorers.get(playerUUID);
             }
         }
 
         if (homeScore > awayScore) {
-            return HomeTeamId;
+            return homeTeamId;
         } else if (homeScore < awayScore)
-            return AwayTeamId;
+            return awayTeamId;
         else
             return null;
     }
