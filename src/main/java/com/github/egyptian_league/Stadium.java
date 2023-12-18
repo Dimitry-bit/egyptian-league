874e5398c3170610d5b8c4c2609c11a9a1eb4251
Tony Medhat <73913361+Dimitry-bit@users.noreply.github.com>
2023-12-19 01:00:34 +0200

    Add stadium getters, and setters

--- src/main/java/com/github/egyptian_league/Stadium.java
+++ src/main/java/com/github/egyptian_league/Stadium.java
@@ -1,29 +1,43 @@
 package com.github.egyptian_league;
 
 import java.time.LocalDate;
-import java.time.LocalDateTime;
 import java.util.ArrayList;
 import java.util.UUID;
 
 import com.github.egyptian_league.json.Annotations.JsonConstructor;
 
-import java.util.Iterator;
-
 public class Stadium {
 
-    public final String StadiumName;
-    public final String StadiumAddress;
-
-    // FIXME: Where can I access it?
     private final UUID stadiumId;
-
+    private String stadiumName;
+    private String stadiumAddress;
     private ArrayList<LocalDate> stadiumSchedule = new ArrayList<>();
 
     @JsonConstructor(parameters = { "StadiumName", "StadiumAddress" })
     public Stadium(String StadiumName, String StadiumAddress) {
-        this.StadiumName = StadiumName;
-        this.StadiumAddress = StadiumAddress;
+        this.stadiumName = StadiumName;
+        this.stadiumAddress = StadiumAddress;
         stadiumId = UUID.randomUUID();
+    }
+
+    public UUID getStadiumUUID() {
+        return stadiumId;
+    }
+
+    public String getStadiumName() {
+        return stadiumName;
+    }
+
+    public void setStadiumName(String stadiumName) {
+        this.stadiumName = stadiumName;
+    }
+
+    public String getStadiumAddress() {
+        return stadiumAddress;
+    }
+
+    public void setStadiumAddress(String stadiumAddress) {
+        this.stadiumAddress = stadiumAddress;
     }
 
     public boolean checkStadiumAvailability(LocalDate date) {
