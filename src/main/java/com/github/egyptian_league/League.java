fa0fc239f7310a0eb6c317d6342ed61465ef0b0a
Tony Medhat <73913361+Dimitry-bit@users.noreply.github.com>
2023-12-19 02:13:36 +0200

    League cleanup

--- src/main/java/com/github/egyptian_league/League.java
+++ src/main/java/com/github/egyptian_league/League.java
@@ -1,33 +1,31 @@
 package com.github.egyptian_league;
+
 import java.time.LocalDate;
 import java.util.ArrayList;
-import java.util.Iterator;
 import java.util.UUID;
 
 public class League {
 
-    private  String LeagueName;
+    public final UUID Id;
 
+    private String LeagueName;
     private int year;
-
-
-
     private final ArrayList<UUID> teams = new ArrayList<>();
-//    private final ArrayList<UUID> players = new ArrayList<>();
     private final ArrayList<UUID> matches = new ArrayList<>();
 
     public League(String LeagueName, int year) {
         setYear(year);
+        this.Id = UUID.randomUUID();
         this.LeagueName = LeagueName;
     }
 
-    public void addMatches(UUID matchId) {
-        matches.add(matchId);
+    public String getLeagueName() {
+        return LeagueName;
     }
 
-//    public void addPlayers(UUID playerId) {
-//        players.add(playerId);
-//    }
+    public void setLeagueName(String leagueName) {
+        LeagueName = leagueName;
+    }
 
     public int getYear() {
         return year;
@@ -43,45 +41,41 @@ public class League {
         this.year = year;
     }
 
-    public void addTeams(UUID teamID) {
-        teams.add(teamID);
+    public ArrayList<Match> getMatches() {
+        ArrayList<Match> matches = new ArrayList<>();
+
+        for (UUID matchId : this.matches) {
+            Match match = ApplicationRepository.getRepository().getMatchByUUID(matchId);
+            matches.add(match);
+        }
+
+        return matches;
     }
 
-//    public void deletePlayer(UUID playerId) {
-//        players.remove(playerId);
-//    }
-
-    public void setLeagueName(String leagueName) {
-        LeagueName = leagueName;
-    }
-
-    public String getLeagueName() {
-        return LeagueName;
+    public void addMatches(UUID matchId) {
+        matches.add(matchId);
     }
 
     public void deleteMatch(UUID matchId) {
         matches.remove(matchId);
     }
 
+    public ArrayList<Team> getTeams() {
+        ArrayList<Team> teams = new ArrayList<>();
+
+        for (UUID teamId : this.teams) {
+            Team team = ApplicationRepository.getRepository().getTeamById(teamId);
+            teams.add(team);
+        }
+
+        return teams;
+    }
+
+    public void addTeams(UUID teamID) {
+        teams.add(teamID);
+    }
+
     public void deleteTeam(UUID teamID) {
         teams.remove(teamID);
     }
-    public Iterator<UUID> getTeams(){
-        return teams.iterator();
-    }
-
-    public ArrayList<UUID> getPlayers() {
-        ArrayList<UUID> players = new ArrayList<>();
-        for (UUID team : teams) {
-            Team obj = ApplicationRepository.getRepository().getTeamById(team);
-            Iterator<UUID> iterator = obj.getPlayers();
-            while (iterator.hasNext()) {
-                players.add(iterator.next());
-            }
-        }
-        return players;
-    }
-        public Iterator<UUID> getMatches () {
-            return matches.iterator();
-        }
-    }
+}
