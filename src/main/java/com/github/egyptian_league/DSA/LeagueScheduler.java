699d6f51c5b4c696bd7b9dbc4223e9cdc5e4ecdb
Tony Medhat <73913361+Dimitry-bit@users.noreply.github.com>
2023-12-19 23:46:06 +0200

    Add LeagueScheduler

---
+++ src/main/java/com/github/egyptian_league/DSA/LeagueScheduler.java
@@ -0,0 +1,73 @@
+package com.github.egyptian_league.DSA;
+
+import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+import java.util.Random;
+
+import com.github.egyptian_league.Models.Match;
+import com.github.egyptian_league.Models.Referee;
+import com.github.egyptian_league.Models.Stadium;
+import com.github.egyptian_league.Models.Team;
+import com.github.egyptian_league.Utils.Pair;
+
+public class LeagueScheduler {
+
+    private final List<Team> teams;
+    private final List<Stadium> stadiums;
+    private final List<Referee> referees;
+    private final List<LocalDateTime> timeSlots;
+
+    public LeagueScheduler(
+            ArrayList<Team> teams,
+            ArrayList<Stadium> stadiums,
+            ArrayList<Referee> referees,
+            ArrayList<LocalDateTime> timeSlots,
+            Random rnd) {
+
+        if (teams.size() % 2 == 1) {
+            throw new IllegalArgumentException("Team count is an odd number (%d)".formatted(teams.size()));
+        }
+
+        this.teams = teams;
+        this.stadiums = stadiums;
+        this.referees = referees;
+        this.timeSlots = timeSlots;
+
+        int timeSlotsCount = timeSlots.size();
+        int mpr = RoundRobin.matchPerRound(teams.size());
+        int round = RoundRobin.rounds(teams.size());
+
+        if ((timeSlotsCount < (mpr * round))) {
+            throw new IllegalArgumentException(
+                    "Time slots count (%d) is less than matches count (%d)".formatted(timeSlotsCount, mpr * round));
+        }
+
+        // Collections.copy(this.teams, teams);
+        Collections.shuffle(this.teams, rnd);
+    }
+
+    public List<Match> generateMatches() {
+        List<ArrayList<Pair<Integer, Integer>>> rawMatches = RoundRobin.circle(teams.size());
+        List<Match> populatedMatches = new ArrayList<>();
+
+        for (int i = 0; i < rawMatches.size(); ++i) {
+            for (int k = 0; k < rawMatches.get(i).size(); ++k) {
+                Pair<Integer, Integer> p = rawMatches.get(i).get(k);
+
+                Team homeTeam = teams.get(p.getKey() - 1);
+                Team awayTeam = teams.get(p.getValue() - 1);
+                Stadium stadium = stadiums.get(i % stadiums.size());
+                Referee referee = referees.get(i % referees.size());
+
+                Match match = new Match(homeTeam.Id, awayTeam.Id, stadium.id, referee.Id,
+                        timeSlots.get((i * (rawMatches.size() - 1)) + k));
+
+                populatedMatches.add(match);
+            }
+        }
+
+        return populatedMatches;
+    }
+}
