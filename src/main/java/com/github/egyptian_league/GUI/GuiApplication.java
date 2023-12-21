5dcc61aa8a3cd19c51f32298695bb18f5d46d16c
Tony Medhat <73913361+Dimitry-bit@users.noreply.github.com>
2023-12-21 03:46:02 +0200

    Add ICON

--- src/main/java/com/github/egyptian_league/GUI/GuiApplication.java
+++ src/main/java/com/github/egyptian_league/GUI/GuiApplication.java
@@ -4,6 +4,7 @@ import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.image.Image;
 import javafx.stage.Stage;
 
 public class GuiApplication extends Application {
@@ -12,6 +13,8 @@ public class GuiApplication extends Application {
         try {
             Parent root = FXMLLoader.load(getClass().getResource("/HomePage.fxml"));
             Scene scene = new Scene(root);
+            stage.getIcons().add(new Image(GuiApplication.class.getResourceAsStream("/5336007-middle.png")));
+            stage.setTitle("Egyptian League");
             stage.setScene(scene);
             stage.show();
         } catch (Exception e) {
